0e80bf04e719cde7016edfa8896f8d4b7f8f1675
Ossi Herrala <oherrala@iki.fi>
2023-01-31 15:24:33 +0200

    Switch to DEFLATE algorithm using miniz_oxide crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,64 +1,46 @@
-use std::io::{self, Write};
+use std::io;
 
-use brotli::enc::BrotliEncoderParams;
+use miniz_oxide::deflate::compress_to_vec;
+use miniz_oxide::inflate::decompress_to_vec;
 use rusqlite::{functions::FunctionFlags, Connection};
 
-pub fn add_brotli_functions(db: &Connection) -> rusqlite::Result<()> {
+pub fn add_deflate_functions(db: &Connection) -> rusqlite::Result<()> {
     db.create_scalar_function(
-        "brotli_compress",
+        "deflate",
         1,
         FunctionFlags::SQLITE_UTF8 | FunctionFlags::SQLITE_DETERMINISTIC,
-        |ctx| brotli_compress(ctx.get_raw(0).as_bytes()?),
+        |ctx| Ok(compress_to_vec(ctx.get_raw(0).as_bytes()?, 6)),
     )?;
 
     db.create_scalar_function(
-        "brotli_decompress",
+        "inflate",
         1,
         FunctionFlags::SQLITE_UTF8 | FunctionFlags::SQLITE_DETERMINISTIC,
-        |ctx| brotli_decompress(ctx.get_raw(0).as_bytes()?),
+        |ctx| decompress_to_vec(ctx.get_raw(0).as_bytes()?).map_err(err_to_rusqlite),
     )?;
 
     Ok(())
 }
 
-fn brotli_compress(input: &[u8]) -> rusqlite::Result<Vec<u8>> {
-    let params = BrotliEncoderParams {
-        quality: 4,
-        ..Default::default()
-    };
-    let mut writer = brotli::CompressorWriter::with_params(Vec::new(), 4096, &params);
-    writer.write_all(input).map_err(io_error_to_rusqlite)?;
-    Ok(writer.into_inner())
-}
-
-fn brotli_decompress(input: &[u8]) -> rusqlite::Result<Vec<u8>> {
-    let mut writer = brotli::DecompressorWriter::new(Vec::new(), 4096);
-    writer.write_all(input).map_err(io_error_to_rusqlite)?;
-    let res = writer.into_inner().map_err(|_| {
-        io_error_to_rusqlite(io::Error::new(
-            io::ErrorKind::Interrupted,
-            "brotli decompression failed",
-        ))
-    })?;
-    Ok(res)
-}
-
-fn io_error_to_rusqlite(err: io::Error) -> rusqlite::Error {
-    rusqlite::Error::UserFunctionError(Box::new(err))
+fn err_to_rusqlite(err: miniz_oxide::inflate::DecompressError) -> rusqlite::Error {
+    rusqlite::Error::UserFunctionError(Box::new(io::Error::new(
+        io::ErrorKind::Other,
+        format!("DEFLATE decompression failed: {:?}", err.status),
+    )))
 }
 
 #[cfg(test)]
 mod tests {
     use rusqlite::Connection;
 
-    use crate::add_brotli_functions;
+    use crate::add_deflate_functions;
 
     #[test]
-    fn test_brotli_compress() -> rusqlite::Result<()> {
+    fn test_deflate() -> rusqlite::Result<()> {
         let db = Connection::open_in_memory()?;
-        add_brotli_functions(&db)?;
+        add_deflate_functions(&db)?;
 
-        let _b = db.query_row(r##"SELECT brotli_compress("aaaaaaaaa")"##, [], |row| {
+        let _b = db.query_row(r##"SELECT deflate("aaaaaaaaa")"##, [], |row| {
             let b: Vec<u8> = row.get(0)?;
             Ok(b)
         })?;
@@ -67,16 +49,16 @@ mod tests {
     }
 
     #[test]
-    fn test_brotli_decompress() -> rusqlite::Result<()> {
+    fn test_inflate() -> rusqlite::Result<()> {
         let db = Connection::open_in_memory()?;
-        add_brotli_functions(&db)?;
+        add_deflate_functions(&db)?;
 
-        db.execute("CREATE TABLE brotli (bloerp);", [])?;
+        db.execute("CREATE TABLE testi (bloerp);", [])?;
         db.execute(
-            "INSERT INTO brotli VALUES(brotli_compress('aaaaaaaaa'));",
+            "INSERT INTO testi VALUES(deflate('aaaaaaaaa'));",
             [],
         )?;
-        let bytes = db.query_row(r"SELECT brotli_decompress(bloerp) FROM brotli", [], |row| {
+        let bytes = db.query_row(r"SELECT inflate(bloerp) FROM testi", [], |row| {
             let bytes: Vec<u8> = row.get(0)?;
             Ok(bytes)
         })?;
@@ -89,14 +71,14 @@ mod tests {
 
     quickcheck::quickcheck! {
         #[test]
-        fn prop_brotli_compress_decompress(xs: String) -> bool {
+        fn prop_deflate_decompress(xs: String) -> bool {
             let db = Connection::open_in_memory().unwrap();
-            add_brotli_functions(&db).unwrap();
+            add_deflate_functions(&db).unwrap();
 
-            db.execute("CREATE TABLE brotli (bloerp);", []).unwrap();
-            db.execute("INSERT INTO brotli VALUES(brotli_compress(?));", [&xs]).unwrap();
+            db.execute("CREATE TABLE testi (bloerp);", []).unwrap();
+            db.execute("INSERT INTO testi VALUES(deflate(?));", [&xs]).unwrap();
 
-            let res: String = db.query_row(r"SELECT brotli_decompress(bloerp) FROM brotli", [], |row| {
+            let res: String = db.query_row(r"SELECT inflate(bloerp) FROM testi", [], |row| {
                 let bytes = row.get(0)?;
                 Ok(String::from_utf8(bytes).unwrap())
             }).unwrap();
